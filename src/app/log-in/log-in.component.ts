7ad6c26176bc0b77690c09b8fb50172043919ad0
Jordan Cotton <Jordancotton55@gmail.com>
2017-04-08 03:35:26 +0300

    Updated log in menu

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, NgForm, Validators, NG_VALIDATORS } from '@angular/forms';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-
+  email: string;
+  password: string;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSignin(form: NgForm) {
+    console.log(form);
+    this.email = form.value.email;
+    this.password = form.value.password;
+    console.log(this.email, this.password);
   }
 
 }
