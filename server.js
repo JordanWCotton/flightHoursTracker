d3e43d3cecc47958ea3071c59312da8f580e7309
Jordan Cotton <Jordancotton55@gmail.com>
2017-07-08 13:31:41 +0300

    Update menu layout and styling

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ let db;
 let currentUser = '';
 
 //Database credentials
-let dbURI = process.env.MONGODB_URI;
+let dbURI = 'mongodb://mongousr7:Password7@ds155820.mlab.com:55820/test-land';
 
 //Connect to database
 mongodb.MongoClient.connect(dbURI, (err, database) => {
